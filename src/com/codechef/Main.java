bc0aa6f90232268d95363c3d5676cf2bee39a77a
Nitin <nsinghboss02@gmail.com>
2021-11-19 11:15:55 +0530

    changed variable name

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// traversal means visiting all the elements of the array once;
     Scanner sc = new Scanner(System.in);
-    int size;
-    size = sc.nextInt();
-    int[] arr = new int[size];
+    int size1;
+    size1 = sc.nextInt();
+    int[] arr = new int[size1];
     for (int i=0;i<arr.length;i++){
         arr[i] = sc.nextInt();
     }
